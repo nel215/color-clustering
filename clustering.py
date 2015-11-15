91ead39139943e0257b6b59a2600e26ae475e269
nel215 <otomo.yuhei@gmail.com>
2015-11-15 20:07:47 +0900

    created color clustering class

--- clustering.py
+++ clustering.py
@@ -2,19 +2,29 @@ import argparse
 import numpy as np
 import cv2
 
+class ColorClustering:
+    def __init__(self):
+        self.K = 16
+
+    def run(self, src, dst):
+        src_img = cv2.imread(src)
+        samples = np.float32(src_img.reshape((-1, 3)))
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        attempts = 3
+        compactness, labels, centers = cv2.kmeans(samples, self.K, criteria, attempts, cv2.KMEANS_PP_CENTERS)
+
+        centers = np.uint8(centers)
+        dst_img = centers[labels.flatten()].reshape(src_img.shape)
+        cv2.imwrite(dst, dst_img)
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--src', required=True)
     parser.add_argument('-d', '--dst', required=True)
     args = parser.parse_args()
 
-    src = cv2.imread(args.src)
-    samples = np.float32(src.reshape((-1, 3)))
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K = 16
-    attempts = 3
-    compactness, labels, centers = cv2.kmeans(samples, K, criteria, attempts, cv2.KMEANS_PP_CENTERS)
+    color_clustering = ColorClustering()
 
-    centers = np.uint8(centers)
-    dst = centers[labels.flatten()].reshape(src.shape)
-    cv2.imwrite(args.dst, dst)
+    color_clustering.run(args.src, args.dst)
+
+
